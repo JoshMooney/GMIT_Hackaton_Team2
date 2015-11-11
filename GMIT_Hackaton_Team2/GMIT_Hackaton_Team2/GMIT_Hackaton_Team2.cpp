a173bba1662e06d68ab02098e4e782e9734733e3
Josh <joshmoo2009@hotmail.com>
2015-11-11 14:41:18 +0000

    Got main game loop working, there is now a SDL window and the renderer has been written and added.

--- GMIT_Hackaton_Team2/GMIT_Hackaton_Team2/GMIT_Hackaton_Team2.cpp
+++ GMIT_Hackaton_Team2/GMIT_Hackaton_Team2/GMIT_Hackaton_Team2.cpp
@@ -3,12 +3,34 @@
 #include <SDL.h>
 #undef main 
 #include "Box2D\Box2D.h"
+using namespace std;
+#include <iostream>
+#include "Renderer.h"
 
-int main()
-{
-	SDL_Rect rect = { 0, 0, 10, 10 };
-	b2Vec2 vector = b2Vec2(100, 100);
-	system("Pause");
-	return 0;
+
+int main(){
+	if (SDL_Init(SDL_INIT_VIDEO) < 0){
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		system("timeout 10");
+		return EXIT_FAILURE;
+	}
+	const int SCREEN_WIDTH = 960;
+	const int SCREEN_HEIGHT = 640;
+	const b2Vec2 GRAVITY = b2Vec2(0, 9.81f);
+	const float box2D_timestep = 1.0f / 60.0f;
+	const int vel_iterations = 6;
+	const int pos_iterations = 2;
+	SDL_Rect worldBounds = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+	Renderer renderer = Renderer(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	bool is_running = true;
+	while (is_running){
+		//Main Game loop here
+		renderer.Begin();
+
+		renderer.End();
+	}
+
+	return EXIT_SUCCESS;
 }
 
